d4544ca86822ddc300813c6cdc0a3a80aaf34199
tjustino <thomas.justino@gmail.com>
2014-05-27 16:56:29 +0200

    better variable

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,14 @@
 class DashboardController < ApplicationController
   def index
-    @credits  = Hash.new
-    @debits   = Hash.new
+    @sum_credits  = Hash.new
+    @sum_debits   = Hash.new
 
     today = Time.now.end_of_day.to_formatted_s(:db)
     past_days = Time.now.months_ago(1).beginning_of_day.to_formatted_s(:db)
 
     @current_accounts.each do |account|
-      @credits[account.id] = account.transactions.where(["amount > 0 and date >= ? and date <= ?", past_days, today]).sum(:amount)
-      @debits[account.id]  = account.transactions.where(["amount < 0 and date >= ? and date <= ?", past_days, today]).sum(:amount)
+      @sum_credits[account.id] = account.transactions.where(["amount > 0 and date >= ? and date <= ?", past_days, today]).sum(:amount)
+      @sum_debits[account.id]  = account.transactions.where(["amount < 0 and date >= ? and date <= ?", past_days, today]).sum(:amount)
     end
   end
 end
