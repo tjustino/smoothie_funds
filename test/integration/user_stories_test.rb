b09d4d94f598154b68e69ccb40de407d7e3b7b58
tjustino <thomas.justino@gmail.com>
2014-06-11 16:07:48 +0200

    enhance integration test

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -26,7 +26,7 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
     assert_template "sessions/new"
     assert_select ".alert-success", 
                   CGI.escapeHTML(I18n.translate('users.create.successfully_created'))
-                  # 
+                  # hack
 
     # login with this user
     post "/login", email: email, password: password
@@ -101,9 +101,58 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
   end
 
   # As a logged user,
-  # I can't go anyway
-  # so that I will be redirected to the dashboard page anytime I do mistake
+  # I can't access data that not mine
+  # so that I will be redirected to the correct page anytime I do mistake
   test "redirect logged user" do
-    
+    good_user   = users(:thomas)
+    wrong_user  = users(:emilie)
+    hacked_name = "Hacked Name"
+
+    # login
+    get login_path
+    post "/login", email: good_user.email, password: "secret"
+    follow_redirect!
+    assert_response :success
+    assert_template "dashboard/index"
+
+    # want to edit another user
+    get edit_user_path(good_user.id)
+    assert_response :success
+    assert_template "users/edit"
+
+    get edit_user_path(wrong_user.id)
+    assert_redirected_to edit_user_path(good_user.id)
+
+    # want to edit another account
+    get user_accounts_path(good_user)
+    assert_response :success
+    assert_template "accounts/index"
+
+    ## index
+    get user_accounts_path(wrong_user)
+    assert_select "ul.nav:nth-child(2) > li:nth-child(1) > a:nth-child(1)", good_user.name
+
+    ## create
+    post user_accounts_path(wrong_user),  user_id: session[:user_id],
+                                          account: {  name: hacked_name,
+                                                      initial_balance: 12.34 }
+    follow_redirect!
+    assert_response :success
+    assert_select "ul.nav:nth-child(2) > li:nth-child(1) > a:nth-child(1)", good_user.name
+    assert good_user.accounts.where(name: hacked_name).any?
+    assert wrong_user.accounts.where(name: hacked_name).empty?
+
+    ## update
+    patch user_account_path(wrong_user, wrong_user.accounts.sample), 
+                                              user_id: session[:user_id],
+                                              account: {  name: hacked_name.reverse,
+                                                          initial_balance: 666 }
+    follow_redirect!
+    assert_response :success
+    assert_select "ul.nav:nth-child(2) > li:nth-child(1) > a:nth-child(1)", good_user.name
+    assert good_user.accounts.where(name: hacked_name.reverse).empty?
+    #puts Account.where(name: hacked_name.reverse).first.users.first.name
+    #assert wrong_user.accounts.where(name: hacked_name.reverse).empty?
+    #puts wrong_user.accounts.where(name: hacked_name.reverse).first.initial_balance
   end
 end
