0dfaa7937b04ea792b68e387428992b2c7aaa8cb
tjustino <thomas.justino@gmail.com>
2014-06-30 17:12:05 +0200

    schedule helper for translating period

---
+++ app/helpers/schedules_helper.rb
@@ -0,0 +1,10 @@
+module SchedulesHelper
+  def easy_t(period)
+    case period
+      when "days"   then t('.days')
+      when "weeks"  then t('.weeks')
+      when "months" then t('.months')
+      when "years"  then t('.years')
+    end
+  end
+end
