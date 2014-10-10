7732a76a3dc4fff42e76cf04df3ad0e226f152c1
tjustino <thomas.justino@gmail.com>
2014-10-10 13:46:42 +0200

    format initial_balance before type cast

--- app/models/account.rb
+++ app/models/account.rb
@@ -12,15 +12,22 @@
 #
 
 class Account < ActiveRecord::Base
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, uniq: true
   has_many                :categories,    dependent: :restrict_with_error
   has_many                :transactions,  dependent: :restrict_with_error
   has_many                :schedules,     dependent: :delete_all
 
+  before_validation :format_initial_balance
+
   validates_presence_of     :name, :initial_balance
   validates_numericality_of :initial_balance
-  #validates_uniqueness_of   :name, scope: :user_id
+  #validates_uniqueness_of   :name, scope: user_id
 
-  #scope :active,       -> { where(active: true) }
   scope :order_by_name, -> { order(name: :asc) }
+  #scope :active,       -> { where(active: true) }
+
+  private
+    def format_initial_balance
+      self.initial_balance = initial_balance_before_type_cast.gsub(',', '.')
+    end
 end
