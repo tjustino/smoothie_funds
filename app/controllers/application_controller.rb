4822e97481176bdb2dd37e80902bcaf26c284ca0
tjustino <thomas.justino@gmail.com>
2014-10-21 14:08:19 +0200

    little cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authorize, :set_current_user, :set_current_accounts
-  #account OK
 
   private
     def current_user
