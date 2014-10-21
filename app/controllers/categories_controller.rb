871e3b85074ab6ea73036ad0bf73538452c32387
tjustino <thomas.justino@gmail.com>
2014-10-21 14:13:38 +0200

    refactoring categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,70 +1,76 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:edit, :update, :destroy]
   before_action :set_current_account
 
   # GET /users/1/accounts/1/categories
   def index
-    @categories = @current_account.categories.order(:name).page(params[:page]).per(20)
+    load_categories
   end
 
   # GET /users/1/accounts/1/categories/new
   def new
-    @category = Category.new
+    build_category
   end
 
   # GET /users/1/accounts/1/categories/1/edit
   def edit
+    load_category
   end
 
   # POST /users/1/accounts/1/categories
   def create
-    @category = Category.new(category_params)
-    @category.account     = @current_account
-    @category.created_by  = @current_user.id
-    @category.updated_by  = @current_user.id
-
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to user_account_categories_url, notice: t('.successfully_created') }
-      else
-        format.html { render :new }
-      end
-    end
+    build_category
+    save_category( t('.successfully_created') ) or render 'new'
   end
 
   # PATCH/PUT /users/1/accounts/1/categories/1
   def update
-    @category.updated_by = @current_user.id
-
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to user_account_categories_url, notice: t('.successfully_updated') }
-      else
-        format.html { render :edit }
-      end
-    end
+    load_category
+    build_category
+    save_category( t('.successfully_updated') ) or render 'edit'
   end
 
   # DELETE /users/1/accounts/1/categories/1
   def destroy
-    respond_to do |format|
-      if @category.destroy
-        format.html { redirect_to user_account_categories_url, notice: t('.successfully_destroyed') }
-      else
-        flash[:warning] = t('.cant_destroy')
-        format.html { redirect_to user_account_categories_url  }
-      end
+    load_category
+    if @category.destroy
+      redirect_to user_account_categories_url, notice: t('.successfully_destroyed')
+    else
+      flash[:warning] = t('.cant_destroy')
+      redirect_to user_account_categories_url
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
+
+  private ######################################################################
+
+    def load_categories
+      #TODO remove kaminari
+      @nb_categories = current_categories.count
+      @categories ||= current_categories.order_by_name.page(params[:page]).per(20)
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def load_category
+      @category ||= current_categories.find(params[:id])
+    end
+
+    def build_category
+      @category ||= current_categories.build
+      @category.attributes = category_params
+    end
+
     def category_params
-      params.require(:category).permit(:name)
+      category_params = params[:category]
+      category_params ? category_params.permit(:name) : {}
+    end
+
+    def save_category(notice)
+      if @category.save
+        userstamp(@category)
+        redirect_to user_account_categories_url, notice: notice
+      end
+    end
+
+    def current_categories
+      @current_account.categories
     end
 end
