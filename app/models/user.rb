0835ae063536029407c26e5522ec6f0c48cb24b1
tjustino <thomas.justino@gmail.com>
2014-10-23 15:52:58 +0200

    name is no longer necessary

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :accounts
 
-  validates_presence_of   :email, :name
+  validates_presence_of   :email
   validates_uniqueness_of :email
   validates_length_of     :password, minimum: 6
 
