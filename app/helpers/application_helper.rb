c6a9c0b35de0e75a714f324e608cd8940baa8703
tjustino <thomas.justino@gmail.com>
2014-10-23 15:57:09 +0200

    email replace name if there is no name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     class_name = current_page?(url) ? 'active' : nil
     content_tag(:li, class: class_name) { link_to fa_icon(fa, text: name), url }
   end
+
+  def name_or_email
+    @current_user.name? ? @current_user.name : @current_user.email
+  end
 end
