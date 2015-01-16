a4e85e7f4171c80e8fed4e5a169027c5e56e58c5
tjustino <thomas.justino@gmail.com>
2015-01-16 13:25:25 +0100

    strong authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authorize, :set_current_user, :set_current_accounts
+  # respect this order for the authorize method that call @current_user
+  before_action :set_current_user, :set_current_accounts, :authorize
 
   private
     def current_user
@@ -15,7 +16,7 @@ class ApplicationController < ActionController::Base
     end
 
     def set_current_accounts
-      @current_accounts = current_user.accounts.active.order_by_name
+      @current_accounts = @current_user.accounts.active.order_by_name if @current_user
     end
 
     def set_current_account
@@ -30,10 +31,58 @@ class ApplicationController < ActionController::Base
     end
 
     def authorize
-      unless  current_user ||
-              ( params[:controller] == "users" && params[:action] == "new" ) ||
-              ( params[:controller] == "users" && params[:action] == "create" )
-        redirect_to login_url
+      # This method only redirect exceptions
+      #
+      #                                +---------------+---------------+
+      #                                | not connected |   connected   |
+      #  +-----------------------------+---------------+---------------+
+      #  | users#new                   |   good url    | dashboard url |
+      #  +-----------------------------+---------------+---------------+
+      #  | users#create                |   good url    | dashboard url |
+      #  +-----------------------------+---------------+---------------+
+      #  | login url                   |   good url    | dashboard url |
+      #  +-----------------------------+---------------+---------------+
+      #  | good user & good account    |   login url   |   good url    |
+      #  +-----------------------------+---------------+---------------+
+      #  | good user & wrong account   |   login url   | dashboard url |
+      #  +-----------------------------+---------------+---------------+
+      #  | wrong user & wrong account  |   login url   | dashboard url |
+      #  +-----------------------------+---------------+---------------+
+      #  | wrong user & good account   |   login url   | dashboard url |
+      #  +-----------------------------+---------------+---------------+
+
+      if user_connected?
+        redirect_to dashboard_url unless (  dashboard_url? or 
+                                            good_user_no_account or
+                                            good_user_good_account )
+      else
+        redirect_to login_url unless ( users_new? or users_create? )
       end
+    end
+
+    def user_connected?
+      not @current_user.blank?
+    end
+
+    def users_new?
+      params[:controller] == "users" && params[:action] == "new"
+    end
+
+    def users_create?
+      params[:controller] == "users" && params[:action] == "create"
+    end
+
+    def dashboard_url?
+      params[:controller] == "dashboard"
+    end
+
+    def good_user_no_account
+      ( params[:user_id].to_s == @current_user.id.to_s and params[:account_id].blank? ) or
+      ( params[:id].to_s == @current_user.id.to_s and params[:controller] == "users" )
+    end
+
+    def good_user_good_account
+      ( params[:user_id].to_s == @current_user.id.to_s if not params[:account_id].blank? ) and
+      ( @current_accounts.ids.to_s.include?(params[:account_id].to_s) if not params[:account_id].blank? )
     end
 end
