cabf5ec0ce5bc2cb29901b45075098889dc56538
tjustino <thomas.justino@gmail.com>
2015-01-19 14:10:47 +0100

    name can be null

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141010125321) do
+ActiveRecord::Schema.define(version: 20150119121857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 20141010125321) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
-    t.string   "name",            null: false
+    t.string   "name"
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
