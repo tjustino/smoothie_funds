f81377f6056f38aea464ef46ebb6895497a64d97
tjustino <thomas.justino@gmail.com>
2015-01-30 16:37:11 +0100

    bundle update

--- app/helpers/dashboard_helper.rb
+++
@@ -1,9 +0,0 @@
-module DashboardHelper
-  def debit_percent(account)
-    ((100 * @sum_debits[account.id].abs) / @sum_credits[account.id]).to_i
-  end
-
-  def credit_percent(account)
-    100 - ((100 * @sum_debits[account.id].abs) / @sum_credits[account.id]).to_i
-  end
-end
