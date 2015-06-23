19197be995e29afe483c1ae92b55ae4ff04e3d9c
tjustino <thomas.justino@gmail.com>
2015-06-23 15:15:02 +0200

    patch logout assert_select

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -5,10 +5,10 @@ class DashboardControllerTest < ActionController::TestCase
   test "should get index" do
     get             :index
     assert_response :success
-    assert_select   'title', "Smoothie Funds"
-    assert_select   'ul.nav:nth-child(2) > li:nth-child(1) > a:nth-child(1)',
-                    @user.name
-    assert_select   'ul.nav:nth-child(2) > li:nth-child(2) > a:nth-child(1)',
+    assert_select   "title", "Smoothie Funds"
+    assert_select   "body > div:nth-child(1) > div.navbar.navbar-default > " +
+                    "div > div.navbar-collapse.collapse > " +
+                    "ul.nav.navbar-nav.navbar-right > li:nth-child(2) > a",
                     I18n.translate('layouts.application.logout')
     assert_not_nil  assigns(:transactions)
     assert_not_nil  assigns(:current_transactions)
