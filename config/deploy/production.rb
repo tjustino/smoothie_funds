433a54d3403cad3eb58e6f5beb416f58e1eb2142
tjustino <thomas.justino@gmail.com>
2015-08-14 14:06:58 +0200

    new server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,4 +5,4 @@ set :rails_env, :production
 set :server_name, "www.smoothiefunds.com smoothiefunds.com"
 set :enable_ssl,  false
 
-server "smoothiefunds.com", user: "www", roles: %w{web app db}, primary: true
+server "vps193113.ovh.net", user: "app", roles: %w{web app db}, primary: true
