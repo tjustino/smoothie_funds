a2a1f445be93cdba8dfd6191bf0c7c214daf374c
tjustino <thomas.justino@gmail.com>
2015-08-31 15:30:43 +0200

    only validate length of password if enterred

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of   :email
   validates_uniqueness_of :email
-  validates_length_of     :password, minimum: 6
+  validates_length_of     :password, minimum: 6, if: :password
 
   has_secure_password
 end
