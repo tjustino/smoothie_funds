4e0d2fb8c0afe53316e804c5e6c88dbccddd35c7
tjustino <thomas.justino@gmail.com>
2015-11-17 17:05:11 +0100

    include db in roles to perform migrations

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-set   :stage,     :production
-set   :rails_env, :production
-role  :web,       %w{app@vps193113.ovh.net}
+set     :stage,     :production
+set     :rails_env, :production
+server  "vps193113.ovh.net", user: "app", roles: %w{web app db}
