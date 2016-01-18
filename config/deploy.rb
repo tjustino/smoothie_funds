b057491cfb26a77a29a5d64796d6c5f0be9090dd
tjustino <thomas.justino@gmail.com>
2016-01-18 13:09:00 +0100

    update to ruby 2.3

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :linked_dirs,   fetch(:linked_dirs, []).push(   "bin",
 
 # capistrano-rbenv
 set :rbenv_type,      :user
-set :rbenv_ruby,      "2.2.3"
+set :rbenv_ruby,      "2.3.0"
 set :rbenv_prefix,    "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins,  %w{rake gem bundle ruby rails}
 
