0f47338896b9b95e73492456509f9dce36645088
tjustino <thomas.justino@gmail.com>
2016-08-26 13:03:13 +0200

    adding some comment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+# app - the Rails application itself
+# db  - the database
+# web - the nginx server that will act as a proxy and serve static assets
 set     :stage,     :production
 set     :rails_env, :production
 server  "vps193113.ovh.net", user: "app", roles: %w{web app db}
