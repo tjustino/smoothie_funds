3342157f0fb2e0b0ecc69313decb51f8a67de7b1
tjustino <thomas.justino@gmail.com>
2017-07-18 14:05:22 +0200

    switch to ruby 2.4.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :linked_dirs,   fetch(:linked_dirs, []).push(   "bin",
 
 # setup rbenv
 set :rbenv_type,      :user
-set :rbenv_ruby,      "2.4.0"
+set :rbenv_ruby,      "2.4.1"
 set :rbenv_prefix,    "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins,  %w{rake gem bundle ruby rails}
 
