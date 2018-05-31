711ba5c152e8a684eb15f02007d1b583ee344a2c
tjustino <thomas.justino@gmail.com>
2018-05-31 16:38:33 +0200

    back to an decent error viewer

--- config/initializers/customize_error.rb
+++ config/initializers/customize_error.rb
@@ -1,23 +1,11 @@
 # frozen_string_literal: true
 
 ActionView::Base.field_error_proc = proc do |html_tag, instance|
-  html        = ""
-  form_fields = %w[textarea input select]
+  class_attr_index = html_tag.index 'class="'
 
-  elements = Nokogiri::HTML::DocumentFragment.parse(html_tag).css "label, " +
-                                             form_fields.join(", ")
- 
-  elements.each do |e|
-    if e.node_name.eql? "label"
-      html = %(#{e}).html_safe
-    elsif form_fields.include? e.node_name
-      e["class"] += " is-invalid"
-      if instance.error_message.is_a?(Array)
-        html = %(#{e}<div class="invalid-feedback">#{instance.error_message.uniq.join(', ')}</div>).html_safe
-      else
-        html = %(#{e}<div class="invalid-feedback">#{instance.error_message}</div>).html_safe
-      end
-    end
+  if class_attr_index
+    html_tag.insert class_attr_index + 7, "is-invalid "
+  else
+    html_tag.insert html_tag.index(">"), ' class="is-invalid"'
   end
-  html
 end
