3cb6331bc2fd5bcc5dd1dba6bfa16917cefac7cc
tjustino <thomas.justino@gmail.com>
2018-08-28 15:19:44 +0200

    use the right domain name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,4 +5,4 @@
 # web - the nginx server that will act as a proxy and serve static assets
 set     :stage,     :production
 set     :rails_env, :production
-server  "vps193113.ovh.net", user: "app", roles: %w[web app db]
+server  "smoothiefunds.com", user: "app", roles: %w[web app db]
