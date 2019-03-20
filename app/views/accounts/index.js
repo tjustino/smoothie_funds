0efefc7ef771b8140cdf144298eef7a7e74326ca
tjustino <thomas.justino@gmail.com>
2019-03-20 17:04:16 +0100

    no more coffe script

--- app/views/accounts/index.js
+++ app/views/accounts/index.js
@@ -1,6 +1,7 @@
-html = "<%= escape_javascript(render partial: 'account', 
+html = "<%= escape_javascript(render partial: 'account',
                                      locals: { accounts: @accounts,
                                                name:     t('.name'),
                                                init_bal: t('.initial_balance'),
                                                share:    t('.share') }) %>"
+
 $("#listing").append(html)
