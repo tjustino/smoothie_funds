1a76f385b8a333f06e694bd985894078d6a4e00d
tjustino <thomas.justino@gmail.com>
2019-05-16 22:16:23 +0200

    goodbye jQuery

--- app/javascript/more_items.js
+++ app/javascript/more_items.js
@@ -5,52 +5,40 @@ var showMeMore = function() {
   var loading    = document.getElementsByClassName('loading')[0];
   var nb_items   = document.getElementById('listing').rows.length;
   var total      = document.getElementById('total');
+  var csrf_token = document.querySelector("meta[name='csrf-token']").getAttribute('content');
+  var action     = document.querySelector('form.button_to').getAttribute('action')
 
   more_items.style.display = 'none';
   loading.style.display = '';
 
-  // ↓ still jQuery ↓
-  return $.ajax({
-    type: "GET",
-    url: $(this).attr("action"),
-    data: {
-      offset: nb_items
-    },
-    dataType: "script",
-    success: function() {
-      $(".loading").hide();
-      if ($("tbody").children("tr").length >= $("#total").text()) {
-        return $("#more_items").hide();
-      } else {
-        return $("#more_items").show();
-      }
-    }
-  });
-  // ↑ still jQuery ↑
+  var request = new XMLHttpRequest();
+  // var timestamp = new Date().getTime()
+  // request.open('GET', document.referrer + '?offset=' + nb_items + '&_=' + timestamp, true);
+  request.open('GET', action + '?offset=' + nb_items, true);
+  request.setRequestHeader('X-CSRF-Token', csrf_token);
+  request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  request.setRequestHeader('Accept', 'text/javascript');
 
-  // var request = new XMLHttpRequest();
-  // request.open('GET', '?offset=' + nb_items, true);
-  //
-  // request.onload = function() {
-  //   if (request.status >= 200 && request.status < 400) {
-  //     loading.style.display = 'none';
-  //     if (nb_items >= total.textContent) {
-  //       more_items.style.display = 'none';
-  //     } else {
-  //       more_items.style.display = '';
-  //     }
-  //
-  //     return request.response;
-  //   } else {
-  //     console.log("We reached our target server, but it returned an error 😵");
-  //   }
-  // };
-  //
-  // request.onerror = function() {
-  //   console.log("There was a connection error 😵");
-  // };
-  //
-  // request.send();
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      loading.style.display = 'none';
+      if (nb_items >= total.textContent) {      // pb dernier appel
+        more_items.style.display = 'none';
+      } else {
+        more_items.style.display = '';
+      }
+
+      eval(this.response);
+    } else {
+      console.log('We reached the target server, but it returned an error 😵');
+    }
+  };
+
+  request.onerror = function() {
+    console.log('There was a connection error 😵');
+  };
+
+  request.send();
 };
 
 once_ready(function(){
