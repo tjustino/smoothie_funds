62734b017db69a823ed9f212be3c944a68a43d37
tjustino <thomas.justino@gmail.com>
2019-05-22 14:08:28 +0200

    config for webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,13 @@
-const { environment } = require("@rails/webpacker")
+const { environment } = require('@rails/webpacker')
+const erb =  require('./loaders/erb')
 
+// specific
 const webpack = require('webpack');
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery'
 }));
+// specific
 
+environment.loaders.prepend('erb', erb)
 module.exports = environment
