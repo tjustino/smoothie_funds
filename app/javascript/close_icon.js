8f88ab54029a810a3e113931b46fe570fefb3f95
tjustino <thomas.justino@gmail.com>
2019-06-03 22:50:56 +0200

    minor cleanup

--- app/javascript/close_icon.js
+++ app/javascript/close_icon.js
@@ -1,7 +1,7 @@
 import { once_ready } from 'once_ready';
 
 once_ready(function(){
-  return $(".close.icon").on("click", function() {
-    return $(this).closest(".message").transition("scale");
+  $(".close.icon").on("click", function() {
+    $(this).closest(".message").transition("scale");
   });
 });
