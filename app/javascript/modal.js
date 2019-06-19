e3c1ca0bbf52d02f5272297f65789e31383dd288
tjustino <thomas.justino@gmail.com>
2019-06-19 21:16:14 +0200

    move categories to semantic-ui

--- app/javascript/modal.js
+++ app/javascript/modal.js
@@ -2,4 +2,8 @@ import { once_ready } from 'once_ready';
 
 once_ready(function(){
   $('#welcomeModal').modal('show');
+
+  $("#importAccountsButton").click(function(){
+    $("#importModal").modal('show');
+  });
 });
