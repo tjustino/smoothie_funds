0ba9ed4c19bf8a549a2b8754c5d89eb937838c29
tjustino <thomas.justino@gmail.com>
2019-07-10 14:49:11 +0200

    fix offense : do not use instance variables in helpers

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -2,15 +2,15 @@
 
 # Analytics Helper
 module AnalyticsHelper
-  def dates_before_past_time(account)
-    transactions_from_past_time_to_now(account).keys.map(&:to_s)
+  def dates_before_past_time(account, transactions)
+    transactions_from_past_time_to_now(account, transactions).keys.map(&:to_s)
   end
 
-  def past_data(account)
-    data = transactions_from_past_time_to_now(account).values
+  def past_data(account, transactions)
+    data = transactions_from_past_time_to_now(account, transactions).values
     data.each_with_index do |amount, index|
       if index.zero?
-        data[index] += balance_before_past_time(account)
+        data[index] += balance_before_past_time(account, transactions)
       else
         data[index] = data[index - 1] + amount
       end
@@ -18,20 +18,20 @@ module AnalyticsHelper
     data.map(&:to_f)
   end
 
-  def balance_before_past_time(account)
-    Account.find(account.id).initial_balance + @transactions
+  def balance_before_past_time(account, transactions)
+    Account.find(account.id).initial_balance + transactions
                                                .where(account: account)
                                                .where("date < ?", past_time)
                                                .sum(:amount)
   end
 
-  def transactions_from_past_time_to_now(account)
-    @transactions.where(account: account)
-                 .where("date >= ?", past_time)
-                 .where("date <= ?", Time.zone.now.midnight)
-                 .group(:date)
-                 .order(date: :asc) # must be before sum
-                 .sum(:amount)
+  def transactions_from_past_time_to_now(account, transactions)
+    transactions.where(account: account)
+                .where("date >= ?", past_time)
+                .where("date <= ?", Time.zone.now.midnight)
+                .group(:date)
+                .order(date: :asc) # must be before sum
+                .sum(:amount)
   end
 
   # def futur_data(account)
