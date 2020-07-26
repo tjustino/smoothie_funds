ea5600e78cbae587810c6bd9f6facc82aea9f878
tjustino <thomas.justino@gmail.com>
2020-07-26 15:19:59 +0200

    remove right DOM level

--- app/javascript/src/close_button.js
+++ app/javascript/src/close_button.js
@@ -3,7 +3,7 @@ import { once_ready } from 'src/once_ready';
 once_ready(function(){
   (document.querySelectorAll('#debug .delete') || []).forEach(
     (to_delete) => {
-      var notification = to_delete.parentNode.parentNode;
+      var notification = to_delete.parentNode.parentNode.parentNode.parentNode;
       to_delete.addEventListener('click', () => {
         notification.parentNode.removeChild(notification);
     });
@@ -13,7 +13,7 @@ once_ready(function(){
 once_ready(function(){
   (document.querySelectorAll('.notification .delete') || []).forEach(
     (to_delete) => {
-      var notification = to_delete.parentNode;
+      var notification = to_delete.parentNode.parentNode.parentNode;
       to_delete.addEventListener('click', () => {
         notification.parentNode.removeChild(notification);
     });
