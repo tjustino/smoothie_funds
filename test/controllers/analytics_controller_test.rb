55976d398f9934ca269c7cd26fa7c66719f34178
tjustino <thomas.justino@gmail.com>
2020-09-17 17:12:15 +0200

    fix test suite

--- test/controllers/analytics_controller_test.rb
+++ test/controllers/analytics_controller_test.rb
@@ -8,9 +8,9 @@ class AnalyticsControllerTest < ActionController::TestCase
   test "should get index" do
     get             :index, params: { user_id: @user }
     assert_response :success
-    assert_select   "h2", I18n.t("analytics.index.analytics")
+    assert_select   "h1", I18n.t("analytics.index.analytics")
     @accounts.order(name: :asc).each_with_index do |account, index|
-      assert_select "#heading#{index} > h3:nth-child(1) > a:nth-child(1)", account.name
+      assert_select "#analytic#{index}", account.name
     end
   end
 end
