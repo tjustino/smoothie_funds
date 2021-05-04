d0e896e93e2a7958e2c4b7e0e75f48bc8cfb99c3
tjustino <thomas.justino@gmail.com>
2021-05-04 22:03:36 +0200

    fix js error

--- app/javascript/show_modal.js
+++ app/javascript/show_modal.js
@@ -1,22 +1,8 @@
-// import { once_ready } from 'once_ready';
-//
-// once_ready(function(){
-//   // ↓ still jQuery - waiting v5: https://github.com/twbs/bootstrap/pull/23586 ↓
-//   $('#welcomeModal').modal('show');
-//   // ↑ still jQuery - waiting v5: https://github.com/twbs/bootstrap/pull/23586 ↑
-// });
+import { once_ready } from 'once_ready';
 
-// var welcomeModal = document.getElementById('welcomeModal')
-// welcomeModal.modal('show');
-
-console.log(bootstrap.Tooltip.VERSION);
-
-let welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
-welcomeModal.show();
-// welcomeModal.focus();
-// var welcomeModal = document.getElementById('welcomeModal');
-// myModal.show(modalToggle);
-// var welcomeModal = document.getElementById('welcomeModal');
-// welcomeModal.addEventListener('show.bs.modal', function (event) {
-//   show();
-// })
+once_ready(function(){
+  if (document.getElementById('welcomeModal') !== null ) {
+    let welcomeModal = new bootstrap.Modal(document.getElementById('welcomeModal'));
+    welcomeModal.show();
+  };
+});
