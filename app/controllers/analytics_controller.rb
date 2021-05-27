a6d82a3ba4d87505c805761ad4500236189ac3ea
tjustino <thomas.justino@gmail.com>
2021-05-27 12:39:18 +0200

    typo

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -4,7 +4,6 @@
 class AnalyticsController < ApplicationController
   # GET /users/:user_id/analytics
   def index
-    @transactions = Transaction.active
-                               .where(account_id: @accounts_with_categories.ids)
+    @transactions = Transaction.active.where(account_id: @accounts_with_categories.ids)
   end
 end
