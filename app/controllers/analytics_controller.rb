ace2df72df61713786f45799b9c7eb9d7c23aedd
tjustino <thomas.justino@gmail.com>
2022-10-25 14:02:17 +0200

    move logic to the controller

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -4,6 +4,36 @@
 class AnalyticsController < ApplicationController
   # GET /users/:user_id/analytics
   def index
-    @transactions = Transaction.active.where(account_id: @accounts_with_categories.ids)
+    account = if params[:account].nil?
+                @accounts_with_categories.first
+              else
+                @current_user.accounts.where(id: params[:account].to_i).first
+              end
+
+    transactions_from_past_time_to_now = Transaction.active
+                                                    .where(account_id: account.id)
+                                                    .where("date >= ?", past_time)
+                                                    .where("date <= ?", Time.zone.now.midnight)
+                                                    .group(:date)
+                                                    .order(date: :asc) # must be before sum
+                                                    .sum(:amount)
+
+    @labels = []
+    transactions_from_past_time_to_now.each_key { |key| @labels << key }
+
+    @data = transactions_from_past_time_to_now.values.map(&:to_f)
+    @data.each_with_index do |amount, index|
+      @data[index] = if index.zero?
+                       Account.find(account.id).initial_balance + amount
+                     else
+                       @data[index - 1] + amount
+                     end
+    end
   end
+
+  private ##############################################################################################################
+
+    def past_time
+      -3.months.from_now.midnight
+    end
 end
