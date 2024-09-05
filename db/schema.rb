6e94ad80a7825791c0c5922dde72d3894d0d83f6
tjustino <thomas@justino.fr>
2024-09-05 15:38:32 +0200

    update to rails 7.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_13_124741) do
+ActiveRecord::Schema[7.2].define(version: 2023_11_13_124741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -107,5 +107,4 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_13_124741) do
     t.datetime "updated_at", precision: nil
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
