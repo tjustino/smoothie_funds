cc7f7bc5c377b399be36f61fefd5f5b934f421c4
tjustino <thomas@justino.fr>
2024-12-09 16:44:55 +0100

    update some packages

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,8 +4,8 @@
 
 pin "application"
 # waiting for a fix → https://github.com/rails/importmap-rails/issues/153
-pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.4/dist/chart.js"
-pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.7/dist/chart.js"
+pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.4/dist/color.esm.js"
 pin "wnumb" # @1.2.0
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
