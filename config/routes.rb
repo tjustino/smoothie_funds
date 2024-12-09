7bfdb84b3d02c9641292b46f271caec919b551ac
tjustino <thomas@justino.fr>
2024-12-09 17:23:53 +0100

    use default Rails root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/*
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
   # By default, there is 7 different paths and 7 actions in each controller:
   # +-----------+------------------+-------------------+
   # | HTTP Verb | Path             | Controller#Action |
