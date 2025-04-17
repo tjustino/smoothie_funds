2598a937ecb2166eed4f6781a0220b62e54edf17
tjustino <thomas@justino.fr>
2025-04-17 10:50:39 +0200

    update chart.js to v4.4.9

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,7 @@
 
 pin "application"
 # waiting for a fix → https://github.com/rails/importmap-rails/issues/153
-pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.7/dist/chart.js"
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.9/dist/chart.js"
 pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.4/dist/color.esm.js"
 pin "wnumb" # @1.2.0
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
