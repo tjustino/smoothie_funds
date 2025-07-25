3d44424e22abb00480193a985e5751d40ceb2573
tjustino <thomas@justino.fr>
2025-07-25 09:22:16 +0200

    fix text decoration behavior

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,9 +1,13 @@
 module TransactionsHelper
+  def is_upcomming?(transaction)
+    "class=upcoming-transaction" if (transaction.date > Time.zone.today)
+  end
+
   def decoration_according_to_(transaction)
     if (transaction.date > Time.zone.today) && (transaction.balance >= 0)
-      "has-text-grey-light upcoming-transaction"
+      "has-text-grey"
     elsif transaction.balance.negative?
-      "has-text-danger upcoming-transaction"
+      "has-text-danger"
     end
   end
 end
